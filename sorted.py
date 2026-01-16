2682f3a95fda7022a2b97afec2be025509185021
shruti <shrutipalankar1201@gmail.com>
2026-01-16 10:14:14 +0530

    Add files via upload

---
+++ sorted.py
@@ -0,0 +1,15 @@
+   
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+data = [78,20,38,35,64,48]
+print(data)
+sorted_data = selection_sort(data)
+print(sorted_data)
